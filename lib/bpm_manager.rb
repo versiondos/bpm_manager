b9a30e7877d41e4f30d0f9701a185b2ef8d9afdd
Ariel Presa <arielpresa@gmail.com>
2015-02-04 22:50:38 +0000

    Bug fixed for config URL

--- lib/bpm_manager.rb
+++ lib/bpm_manager.rb
@@ -28,11 +28,6 @@ module BpmManager
   
   # Returns the URI for the server plus a suffix
   def self.uri(rest_service = '')
-    case configuration.bpm_vendor.downcase
-      when 'redhat'
-        URI.encode('http' + (configuration.bpm_use_ssl ? 's' : '') + '://' + configuration.bpm_username + ':' + configuration.bpm_password + '@' + configuration.bpm_url + configuration.bpm_url_suffix + (rest_service.nil? ? '' : rest_service))
-      else
-        ''
-    end
+    URI.encode('http' + (configuration.bpm_use_ssl ? 's' : '') + '://' + configuration.bpm_username + ':' + configuration.bpm_password + '@' + configuration.bpm_url + configuration.bpm_url_suffix + (rest_service.nil? ? '' : rest_service))
   end
 end
