7b8fd3532cabc3444dfc70e60a3c56e700725b6d
Ariel Presa <arielpresa@gmail.com>
2015-02-05 18:34:23 +0000

    Added Start, Stop, Skip, Suspend, Resume, Exit, Fail functions for RedHat

--- lib/bpm_manager/red_hat.rb
+++ lib/bpm_manager/red_hat.rb
@@ -18,17 +18,17 @@ module BpmManager
     def self.tasks_with_opts(opts = {})
       self.structure_task_data(JSON.parse(RestClient.get(BpmManager.uri('/task/query' + (opts.empty? ? '' : '?' + opts.map{|k,v| k.to_s + '=' + v.to_s}.join('&'))), :accept => :json)))
     end
-  
+    
     # Gets all Process Instances
     def self.process_instances
       JSON.parse(RestClient.get(BpmManager.uri('/history/instances'), :accept => :json))
     end
-  
+    
     # Gets a Process Instance
     def self.process_instance(process_instance_id)
       JSON.parse(RestClient.get(BpmManager.uri('/history/instance/' + process_instance_id.to_s), :accept => :json))
     end
-  
+    
     # Gets a Process Instance Variables
     def self.process_instance_variables(deployment_id, process_instance_id)
       JSON.parse(RestClient.get(BpmManager.uri('/runtime/' + deployment_id.to_s + '/withvars/process/instance/' + process_instance_id.to_s), :accept => :json))['variables']
@@ -38,15 +38,50 @@ module BpmManager
     def self.assign_task(task_id, user_id)
       RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/delegate?targetEntityId=' + user_id.to_s)), :headers => {:content_type => :json, :accept => :json})
     end
-  
+    
+    # Starts a Task
+    def self.start_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/start')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
+    # Stops a Task
+    def self.stop_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/stop')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
+    # Suspends a Task
+    def self.suspend_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/suspend')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
+    # Resumes a Task
+    def self.suspend_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/resumes')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
     # Releases a Task
     def self.release_task(task_id)
       RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/release')), :headers => {:content_type => :json, :accept => :json})
     end
-  
+    
+    # Skips a Task
+    def self.release_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/skip')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
     # Completes a Task
-    def self.complete_task(task_id)
+    def self.complete_task(task_id, opts = {})
       RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/complete')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
+    # Fails a Task
+    def self.complete_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/fail')), :headers => {:content_type => :json, :accept => :json})
+    end
+    
+    # Exits a Task
+    def self.complete_task(task_id)
+      RestClient.post(URI.encode(BpmManager.uri('/task/' + task_id.to_s + '/exit')), :headers => {:content_type => :json, :accept => :json})
     end
     
     private
@@ -57,7 +92,6 @@ module BpmManager
           input['taskSummaryList'].each do |task|
             my_task = OpenStruct.new
             my_task.process = OpenStruct.new
-            
             my_task.id = task['task-summary']['id']
             my_task.process_instance_id = task['task-summary']['process-instance-id']
             my_task.parent_id = task['task-summary']['parent_id']
@@ -78,7 +112,6 @@ module BpmManager
             my_task.process.version = my_task.process.data['process-version']
             my_task.process.creator = my_task.process.data['identity']
             my_task.process.variables = self.process_instance_variables(my_task.process.deployment_id, my_task.process.instance_id)
-            
             tasks << my_task
           end
         end
