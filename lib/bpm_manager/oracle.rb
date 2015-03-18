4111ea06c4aa80244ed5abf63b20bcb70dabad0f
Ariel Presa <arielpresa@gmail.com>
2015-03-18 19:11:04 +0000

    More tweaks and fixes for hashes

--- lib/bpm_manager/oracle.rb
+++ lib/bpm_manager/oracle.rb
@@ -110,14 +110,14 @@ module BpmManager
             my_task.description = ''
             my_task.data = process_info
             
-            my_task.process.data = ''
+            my_task.process.data = process_info
             my_task.process.deployment_id = ''
-            my_task.process.id = process_info['processId']
+            my_task.process.id = process_info.empty? ? '' : process_info[0]
             
             my_task.process.instance_id = task['processInstanceId']
             my_task.process.start_on = Date.parse(task['created_on'])
             my_task.process.name = task['processName']
-            my_task.process.version = process_info['revision']
+            my_task.process.version = process_info.empty? ? '' : process_info[6]
             my_task.process.creator = 'Not defined'
             my_task.process.variables = '' # self.process_instance_variables(my_task.process.deployment_id, my_task.process.instance_id)
             tasks << my_task
