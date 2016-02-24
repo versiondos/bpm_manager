51171975fa2e0a32a21f41ffdd2153ce53a73700
Ariel Presa <arielpresa@gmail.com>
2016-02-24 20:24:07 +0000

    Minor tweaks in time manipulation

--- lib/bpm_manager/red_hat.rb
+++ lib/bpm_manager/red_hat.rb
@@ -148,18 +148,17 @@ module BpmManager
     # Private class methods
     def self.calculate_sla(start, sla_hours = 0.0, offset = 20)
       sla_hours = sla_hours.to_f * 3600   # converts to seconds
-
-      sla_hours > 0 ? (start + sla_hours) * ((100 - offset) / 100) <= Time.now.utc ? 0 : (start + sla_hours <= Time.now.utc ? 1 : 2) : 0
+      sla_hours > 0 ? Time.at((start.utc + sla_hours).to_f * ((100 - offset) / 100)) <= Time.now.utc ? 0 : (start.utc + sla_hours <= Time.now.utc ? 1 : 2) : 0
     end
     private_class_method :calculate_sla
     
     def self.calculate_sla_percent(start, sla_hours=0, offset=20)
       sla_hours = sla_hours.to_f * 3600   # converts to seconds
       percent = OpenStruct.new
-      total = Time.now.utc - start
+      total = Time.now.utc - start.utc
       
-      percent.green  = sla_hours > 0 ? ((start + sla_hours) * (100 - offset) / 100) * 100 / total : 100
-      percent.yellow = sla_hours > 0 ? ((start + sla_hours) * 100 / total) - percent.green : 0
+      percent.green  = sla_hours > 0 ? ((start.utc + sla_hours).to_f * (100 - offset) / 100) * 100 / total : 100
+      percent.yellow = sla_hours > 0 ? ((start.utc + sla_hours).to_f * 100 / total) - percent.green : 0
       percent.red    = sla_hours > 0 ? 100 - percent.yellow - percent.green : 0
       
       return percent
