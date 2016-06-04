a79cdb4680b7296b1f1936a258e7cd896ea2bb6b
Ariel Presa <arielpresa@gmail.com>
2016-06-04 05:23:00 +0000

    REST calls changes for GET/POST

--- lib/bpm_manager.rb
+++ lib/bpm_manager.rb
@@ -1,6 +1,7 @@
 require "bpm_manager/version"
 require "bpm_manager/red_hat"
 require "bpm_manager/oracle"
+require "rest-client"
 
 module BpmManager
   class << self
@@ -29,5 +30,10 @@ module BpmManager
   # Returns the URI for the server plus a suffix
   def self.uri(rest_service = '')
     URI.encode('http' + (configuration.bpm_use_ssl ? 's' : '') + '://' + configuration.bpm_username + ':' + configuration.bpm_password + '@' + configuration.bpm_url + configuration.bpm_url_suffix + (rest_service.nil? ? '' : rest_service))
+  end
+  
+  # Returns the Rest Client private resource
+  def self.server
+    RestClient::Resource.new(URI.encode('http' + (configuration.bpm_use_ssl ? 's' : '') + '://' + configuration.bpm_url + configuration.bpm_url_suffix), :user => configuration.bpm_username, :password => configuration.bpm_password, :headers => {:content_type => :json, :accept => :json})
   end
 end
